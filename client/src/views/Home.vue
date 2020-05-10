85a1ebcb7708be74ff55f9326f00c8ca649eeea3
Patryk Loter <patryk.loter@gmail.com>
2020-05-10 18:29:36 +0100

    project init

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Home Page
+    <br>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Home',
+})
+</script>
+
+<style>
+
+</style>
