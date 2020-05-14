815b189539c7c75680d0fc55fa88760d0b0bf1f9
Patryk Loter <patryk.loter@gmail.com>
2020-05-14 15:43:33 +0100

    run test in pipeline

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 LC_CTYPE=en_US.utf8
 
 cd client
-npm run start > ../logs/frontend.log & cd ..
+npm run start > $PWD/logs/frontend.log & cd ..
 cd server 
 deno install --unstable --allow-read --allow-run -f https://deno.land/x/denon/denon.ts
-denon index.ts >../logs/backend.log
+denon index.ts >$PWD/logs/backend.log
