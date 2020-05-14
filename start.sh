5bc35a0c9b0a1e7b47be0da0695d2590371d4713
Patryk Loter <patryk.loter@gmail.com>
2020-05-14 18:10:34 +0100

    deno version upgrade

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ LC_CTYPE=en_US.utf8
 cd client
 npm run start > $PWD/logs/frontend.log & cd ..
 cd server 
-deno install --unstable --allow-read --allow-run -f https://deno.land/x/denon/denon.ts
 deno run --allow-net --allow-read --allow-write --config=tsconfig.json index.ts >$PWD/logs/backend.log
