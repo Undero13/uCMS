3ac6f166525191bbeffc28458bd1a7dccf30905a
Patryk Loter <patryk.loter@gmail.com>
2020-05-27 20:12:46 +0100

    add test file to project

---
+++ client/src/views/Login/Login.vue.test.ts
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils';
+import Login from "./Login.view.vue";
+
+test('it can be mount', () => {
+  const wrapper = shallowMount(Login);
+
+  expect(wrapper).toBeInstanceOf(Object);
+});
+
+test('it have two children', () => {
+  const wrapper = shallowMount(Login);
+
+  expect(wrapper.html().includes("<header-stub>")).toBeTruthy();
+  expect(wrapper.html().includes("<login-form-stub>")).toBeTruthy();
+});
