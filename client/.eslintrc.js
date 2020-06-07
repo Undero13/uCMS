4a57dba6349bc39130ddd47a1788a632c2027277
Patryk Loter <patryk.loter@gmail.com>
2020-06-07 16:19:55 +0100

    add stylelint and prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  "parserOptions": {
-    "project": "./tsconfig.json"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'airbnb-typescript',
-    'prettier/@typescript-eslint'
-  ],
-  "env": {
-    "jest": true
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-typescript"],
+  env: {
+    jest: true,
   },
   rules: {
     "@typescript-eslint/camelcase": "off",
-    "semi": [2, "always"],
+    semi: [2, "always"],
     "import/extensions": "off",
     "class-methods-use-this": "off",
-    "no-return-assign": "off"
-  }
+    "no-return-assign": "off",
+    //prettier work
+    "comma-dangle": "off",
+    "operator-linebreak": "off",
+    "@typescript-eslint/quotes": "off",
+  },
 };
