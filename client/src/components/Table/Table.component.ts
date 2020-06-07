cc5dbe4caf66a00417647f75e86d5a2c07a81e96
Patryk Loter <patryk.loter@gmail.com>
2020-06-07 18:21:43 +0100

    add table component

---
+++ client/src/components/Table/Table.component.ts
@@ -0,0 +1,11 @@
+import { defineComponent } from "@vue/runtime-dom";
+
+export default defineComponent({
+  name: "Table",
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+});
