3f5064362cfe5f6708d5d3c94b3a2aa362cd6dc6
Patryk Loter <patryk.loter@gmail.com>
2020-06-15 18:33:32 +0100

    backend - user passwordchange test

---
+++ server/test/acceptance/User/UserResetPassword.test.ts
@@ -0,0 +1,27 @@
+import { assertEquals } from "../../../deno_modules.ts";
+import UserHelper from "../Helpers/UserHelper.ts";
+
+const baseUrl = "http://localhost:3000/api/user/reset-password";
+
+Deno.test("[http] user.resetpassword.valid", async () => {
+  const user = await UserHelper.createRandomUser();
+
+  const requestArgument = {
+    token: user.token,
+    password: "testPassword1234",
+    remindPassword: "testPassword1234",
+  };
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(requestArgument),
+  });
+
+  const data = await response.json();
+
+  assertEquals(response.status, 200);
+  assertEquals(
+    data,
+    { status: true, error: "", data: [] },
+  );
+});
