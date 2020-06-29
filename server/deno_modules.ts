3b7ff8955c0e0fdc3ee93a37adb58fb693198915
Patryk Loter <patryk.loter@gmail.com>
2020-06-29 16:22:40 +0100

    backend - deno update

--- server/deno_modules.ts
+++ server/deno_modules.ts
@@ -13,7 +13,7 @@ export {
   Content,
   HookTarget,
   UseHook,
-} from "https://deno.land/x/alosaur@v0.19.3/mod.ts";
+} from "https://deno.land/x/alosaur@v0.19.4/mod.ts";
 
 export {
   makeJwt,
@@ -26,7 +26,7 @@ export {
   validateJwt,
   parseAndDecode,
 } from "https://deno.land/x/djwt@v0.9.0/validate.ts";
-export { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+export { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 export { uuid } from "https://deno.land/x/uuid@v0.1.2/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
